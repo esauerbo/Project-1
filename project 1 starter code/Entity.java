5735ef770d51554b1e375fa3f5616f6ada129f90
esauerbo <esauerbo@calpoly.edu>
2020-10-25 14:30:47 -0700

    created octo class

--- project 1 starter code/Entity.java
+++ project 1 starter code/Entity.java
@@ -8,16 +8,16 @@ Entity ideally would includes functions for how all the entities in our virtual 
  */
 
 
-final class Entity
+public abstract class Entity
 {
-   public static final String OCTO_KEY = "octo";
+  /* public static final String OCTO_KEY = "octo";
    public static final int OCTO_NUM_PROPERTIES = 7;
    public static final int OCTO_ID = 1;
    public static final int OCTO_COL = 2;
    public static final int OCTO_ROW = 3;
    public static final int OCTO_LIMIT = 4;
    public static final int OCTO_ACTION_PERIOD = 5;
-   public static final int OCTO_ANIMATION_PERIOD = 6;
+   public static final int OCTO_ANIMATION_PERIOD = 6;*/
    public static final String FISH_KEY = "fish";
    public static final int FISH_NUM_PROPERTIES = 5;
    public static final int FISH_ID = 1;
---
+++ project 1 starter code/Octo.java
@@ -0,0 +1,22 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Octo extends Entity {
+
+    public static final String OCTO_KEY = "octo";
+    public static final int OCTO_NUM_PROPERTIES = 7;
+    public static final int OCTO_ID = 1;
+    public static final int OCTO_COL = 2;
+    public static final int OCTO_ROW = 3;
+    public static final int OCTO_LIMIT = 4;
+    public static final int OCTO_ACTION_PERIOD = 5;
+    public static final int OCTO_ANIMATION_PERIOD = 6;
+
+    public Octo(String id, Point position,
+                List<PImage> images, int resourceLimit, int resourceCount,
+                int actionPeriod, int animationPeriod){
+        super(id, position, images, resourceLimit, resourceCount, actionPeriod, animationPeriod);
+    }
+}
+
